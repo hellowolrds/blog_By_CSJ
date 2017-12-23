4706ad7fb402bf54d33ec703b990709a238fdd57
tomsj <csj504@qq.com>
2017-12-23 18:34:27 +0800

    文章添加和编辑功能完成

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -2,7 +2,9 @@
 namespace app\admin\controller;
 use think\Controller;
 use think\Session;
-
+use think\Db;
+use think\Request;
+use think\Paginator;
 class Article extends Controller {
 	public function _initialize() {
 		if ( !Session::has('username') ) {
@@ -11,15 +13,88 @@ class Article extends Controller {
 	}
 
 	public function index () {
-		return view('index');
+		$count = Db::table('article')->count();
+		$articles = Db::table('article')->join('article_sort', 'article.tab_id=article_sort.id')->paginate(2);
+		return $this->fetch('index', ['count'=>$count, 'articles'=>$articles]);
 	}
 
-	public function add () {
-		return view('add');
+	public function add (Request $request) {
+
+		if ( !$request->isPOST() ) {
+			// 获取栏目
+			$tabs = Db::table('article_sort')->select();
+			return $this->fetch('add', ['tab'=>$tabs, 'title'=>'撰写新文章', 'url'=>"/admin/Article/add"]);
+		}
+		$title = input('post.title');
+		$content = input('post.content');
+		$keyword = input('post.keywords');
+		$describe = input('post.describe');
+		$category = input('post.category');
+		$tags = input('post.tags');
+		$titlepic = input('post.titlepic');
+		$lock = input('post.visibility');
+
+		$data['article_name'] = $title;
+		$data['article_time'] = time();
+		$data['article_ip'] = $request->ip();
+		$data['article_click'] = 0;
+		$data['sort_article_id'] = 0;
+		$data['tab_id'] = $category;
+		$data['article_content'] = $content;
+		$data['tags'] = $tags;
+		$data['image_url'] = $titlepic;
+		$data['lock'] = $lock;
+		$data['keywords'] = $keyword;
+		$data['description'] = $describe;
+
+		$result = Db::table('article')->insert($data);
+
+		if ( !$result ) {
+			$this->error("添加失败");
+		}
+		return $this->success("添加成功");
 	}
 
-	public function edit () {
-		return view('edit');
+	public function edit ($id,Request $request) {
+		if ( !$request->isPOST() ) {
+			// 获取栏目
+			$tabs = Db::table('article_sort')->select();
+			$article = Db::table('article')->where('article_id',$id)->find();
+			return $this->fetch('edit', ['tab'=>$tabs, 'title'=>'编辑文章', 'url'=>"/admin/Article/update", 'article'=>$article]);
+		}
+		
+	}
+
+	public function update (Request $request) {
+		$title = input('post.title');
+		$content = input('post.content');
+		$keyword = input('post.keywords');
+		$describe = input('post.describe');
+		$category = input('post.category');
+		$tags = input('post.tags');
+		$titlepic = input('post.titlepic');
+		$lock = input('post.visibility');
+		$id = input('post.id');
+
+		$data['article_name'] = $title;
+		$data['article_time'] = time();
+		$data['article_ip'] = $request->ip();
+		$data['article_click'] = 0;
+		$data['sort_article_id'] = 0;
+		$data['tab_id'] = $category;
+		$data['article_content'] = $content;
+		$data['tags'] = $tags;
+		$data['image_url'] = $titlepic;
+		$data['lock'] = $lock;
+		$data['keywords'] = $keyword;
+		$data['description'] = $describe;
+
+		$result = Db::table('article')->where('article_id',$id)->update($data);
+
+		if ( !$result ) {
+			$this->error("更新失败");
+		}
+		return $this->success("更新成功", "/admin/Article/index");
 	}
 
 	public function del () {
--- application/admin/validate/Article.php
+++ application/admin/validate/Article.php
@@ -3,5 +3,7 @@ namespace app\admin\validate;
 use think\Validate;
 
 class Article extends Validate {
-	
+	protected $rule = [
+		'article_name' => 'require',
+	];
 }
